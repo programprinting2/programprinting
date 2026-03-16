49ee5ae61b61770f5b03ef2f6527f85eb2bdc092
CoreX20 <korneliussetiawan13@gmail.com>
2026-03-16 16:42:32 +0700

    fix error url image placeholder on header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -194,12 +194,12 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://via.placeholder.com/30x30') }}" alt="profile">
+          <img class="wd-30 ht-30 rounded-circle" src="{{ url('https://placehold.co/30x30') }}" alt="profile">
         </a>
         <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
           <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
             <div class="mb-3">
-              <img class="wd-80 ht-80 rounded-circle" src="{{ url('https://via.placeholder.com/80x80') }}" alt="">
+              <img class="wd-80 ht-80 rounded-circle" src="{{ url('https://placehold.co/80x80') }}" alt="">
             </div>
             <div class="text-center">
               <p class="tx-16 fw-bolder">Amiah Burton</p>
