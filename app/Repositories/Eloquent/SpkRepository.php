dda125a7d34a5f5702f60cbd6067b6f618d19e79
CoreX20 <korneliussetiawan13@gmail.com>
2026-03-16 17:03:49 +0700

    add total_biaya to SPK query select

--- app/Repositories/Eloquent/SpkRepository.php
+++ app/Repositories/Eloquent/SpkRepository.php
@@ -34,7 +34,7 @@ class SpkRepository implements SpkRepositoryInterface
         //     'items.produk.bahanBakus:id,nama_bahan,kode_bahan'
         // ]);
         $query = $this->model
-            ->select('id','nomor_spk','pelanggan_id','tanggal_spk','status','created_at')
+            ->select('id','nomor_spk','pelanggan_id','tanggal_spk','status','created_at','total_biaya')
             ->with($with);
 
 
